257cf3a7baefd09fd69123519894717028041315
Franco du Plessis <francodplessis@outlook.com>
2024-08-22 01:36:15 +0200

    Simple UI design

--- excel-print-to-pdf-app/Form1.Designer.vb
+++ excel-print-to-pdf-app/Form1.Designer.vb
@@ -22,10 +22,74 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        rbtn_printAll = New RadioButton()
+        rbtn_printFirstSheet = New RadioButton()
+        btn_Print = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(12, 12)
+        Label1.Margin = New Padding(3)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(119, 17)
+        Label1.TabIndex = 0
+        Label1.Text = "Print Excel to PDF"
+        ' 
+        ' rbtn_printAll
+        ' 
+        rbtn_printAll.AutoSize = True
+        rbtn_printAll.Location = New Point(12, 35)
+        rbtn_printAll.Name = "rbtn_printAll"
+        rbtn_printAll.Size = New Size(101, 19)
+        rbtn_printAll.TabIndex = 1
+        rbtn_printAll.TabStop = True
+        rbtn_printAll.Text = "Print all sheets"
+        rbtn_printAll.UseVisualStyleBackColor = True
+        ' 
+        ' rbtn_printFirstSheet
+        ' 
+        rbtn_printFirstSheet.AutoSize = True
+        rbtn_printFirstSheet.Location = New Point(128, 35)
+        rbtn_printFirstSheet.Name = "rbtn_printFirstSheet"
+        rbtn_printFirstSheet.Size = New Size(104, 19)
+        rbtn_printFirstSheet.TabIndex = 2
+        rbtn_printFirstSheet.TabStop = True
+        rbtn_printFirstSheet.Text = "Print first sheet"
+        rbtn_printFirstSheet.UseVisualStyleBackColor = True
+        ' 
+        ' btn_Print
+        ' 
+        btn_Print.Location = New Point(12, 64)
+        btn_Print.Name = "btn_Print"
+        btn_Print.Size = New Size(220, 31)
+        btn_Print.TabIndex = 3
+        btn_Print.Text = "Print"
+        btn_Print.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(244, 107)
+        Controls.Add(btn_Print)
+        Controls.Add(rbtn_printFirstSheet)
+        Controls.Add(rbtn_printAll)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.FixedDialog
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Excel to PDF"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents rbtn_printAll As RadioButton
+    Friend WithEvents rbtn_printFirstSheet As RadioButton
+    Friend WithEvents btn_Print As Button
 
 End Class
