5f26e220d7e845b33e84dcb4cbe00450fc724ab0
Franco du Plessis <francodplessis@outlook.com>
2024-08-22 02:11:40 +0200

    Updated Form1.vb logic: - created an constructor to check if the filesToPrint folder is present and creates it, if not. - call the respective subroutines based on which radio button is selected when the print button is clicked.

--- excel-print-to-pdf-app/Form1.vb
+++ excel-print-to-pdf-app/Form1.vb
@@ -1,3 +1,30 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Dim fso = CreateObject("Scripting.FileSystemObject")
+        Dim fileFolder = fso.GetAbsolutePathName(".") + "\filesToPrint\"
+        If Not Directory.Exists(fileFolder) Then
+            Directory.CreateDirectory(fileFolder)
+        End If
+
+    End Sub
+
+    Private Sub btn_Print_Click(sender As Object, e As EventArgs) Handles btn_Print.Click
+
+        If rbtn_printAll.Checked() Then
+            PrintServices.XLS_print_to_PDF()
+            MessageBox.Show("Finished")
+        ElseIf rbtn_printFirstSheet.Checked() Then
+            PrintServices.XLS_print_first_sheet_to_PDF()
+            MessageBox.Show("Finished")
+        End If
+
+    End Sub
 End Class
